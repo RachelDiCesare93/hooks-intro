12170940e3f63ffff472ae0033f79441a5375f9b
Rachel DiCesare <racheldicesare@Rachels-iMac.attlocal.net>
2020-10-31 15:38:25 -0500

    first commit

---
+++ src/AppClass.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+  
+  render() {
+  return (
+    <div>
+    <button onClick={this.incrementCount}>
+      I was clicked {this.state.count} times
+    </button>
+    </div>
+  );
+}
+}
+
+export default App;
